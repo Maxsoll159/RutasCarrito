9da8852353d9e0a2ae1e80f943761ab2b479dbac
Maxsoll159 <mar_1905@hotmail.es>
2023-06-13 22:53:27 -0500

    useState

--- src/Micomponent.jsx
+++ src/Micomponent.jsx
@@ -1,20 +1,24 @@
 import { SegundoComponente } from "./SegundoComponente";
+import { HookUseState } from "./components/HookUseState";
 export const Micomponent = () => {
   const miNombre = "Martin";
-  const personajes={
+  const personajes = {
     id: 1,
     nombre: "Tony Stark",
-    alias: "Iroman"
-  }
-  const nombre2 = "Pepe Lucho"
+    alias: "Iroman",
+  };
+  const nombre2 = "Pepe Lucho";
 
   //PROPS
   return (
     <>
-      <SegundoComponente nombre={miNombre} personajes={personajes}  />
+      <HookUseState />
+      {/*
+          <SegundoComponente nombre={miNombre} personajes={personajes}  />
       <h1>Hola</h1>
       <h2>Juan</h2>
       <SegundoComponente nombre={nombre2} personajes={personajes} />
+    */}
     </>
   );
 };
---
+++ src/components/HookUseState.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+
+export const HookUseState = () =>{
+    const [count, setCount] = useState(0)
+    const [nombre, setNombre] = useState("Martin")
+    const [personajes, setPesonajes] = useState({
+        id: 10,
+        nombrePer: "Lucho",
+        edad: 45
+    })
+    const {id, nombrePer, edad} = personajes
+    const sumar = () =>{
+        setCount(count + 1)
+    }
+    const restar = () =>{
+        setCount(count - 1)
+    }
+    const reset = () =>{
+        setCount(0)
+    }
+    const cambiar = () =>{
+        setNombre("Pepe")
+    }
+    return(
+        <div>
+            <h1>Soy el Hook Use State</h1>
+            <p>El numero es {count}</p>
+            <button onClick={sumar}>Sumar + 1</button>
+            <button onClick={restar}>Resto - 1</button>
+            <button onClick={reset}>Resetar</button>
+
+            <h2>Mi nombre es {nombre}</h2>
+            <button onClick={cambiar}>Cambiar</button>
+
+            <p>El personjes es {nombrePer}</p>
+        </div>
+    )
+}
